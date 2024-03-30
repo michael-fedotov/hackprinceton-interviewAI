335e752fa973af4d7ff410e0268d651eaff351c7
jacob-cardoso <jacobcardoso2003@gmail.com>
2024-03-30 17:29:25 -0400

    Added initial django code.

---
+++ first_project/first_app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    tv_shows_list={"tv_shows":{'Game of Thrones':'9.3','Blacklist': '8','Suits': '8.5','The Witcher': '8.5'}}
+    return render(request,'first_app/index.html',context=tv_shows_list)
+
+def home(request):
+    return HttpResponse("Welcome to home page!")
+
+def educative(request):
+    return HttpResponse("Welcome to Educative page!")
